c51d17227445c0c530026c3edb264200222e0875
Sohaib Raheem <miansohaibraheem@gmail.com>
2019-10-24 14:31:31 +0500

    Create hacks.php

---
+++ hacks.php
@@ -0,0 +1,18 @@
+<?php
+$queried_object = get_queried_object();
+$selected_cat = $queried_object->term_id;
+?>
+<select name="event-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'> 
+    <?php 
+        $option = '<option value="' . get_option('home') . '/maskinpark/">All Categories</option>';
+        $categories = get_categories(); 
+        foreach ($categories as $category) {
+			$selected = ( $category->term_id == esc_attr( $selected_cat ) ) ? 'selected' : '';
+            $option .= '<option '.$selected.' value="'.get_option('home').'/category/'.$category->slug.'">';
+            $option .= $category->cat_name;
+            $option .= ' ('.$category->category_count.')';
+            $option .= '</option>';
+        }
+        echo $option;
+    ?>
+</select>
