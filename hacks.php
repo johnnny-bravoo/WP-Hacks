82c8cca8bcf2ac5dda10e66d6c884a3b6728ee33
Sohaib Raheem <miansohaibraheem@gmail.com>
2019-11-14 18:09:07 +0500

    Update hacks.php

--- hacks.php
+++ hacks.php
@@ -18,3 +18,39 @@ $selected_cat = $queried_object->term_id;
         echo $option;
     ?>
 </select>
+
+
+
+/* WP Custom quries for SELECT and UPDATE */
+
+$querystr = "
+    SELECT *
+    FROM $wpdb->posts wposts, $wpdb->postmeta metadate, $wpdb->postmeta metatime
+    WHERE (wposts.ID = metadate.post_id AND wposts.ID = metatime.post_id)
+    AND (metadate.meta_key = 'tf_events_startdate' AND metadate.meta_value > $yesterday )
+    AND metatime.meta_key = 'tf_events_starttime'
+    AND wposts.post_type = 'tf_events'
+    AND wposts.post_status = 'publish'
+    ORDER BY metadate.meta_value ASC, metatime.meta_value DESC LIMIT 30
+ ";
+
+$update = "UPDATE wp207_postmeta wm 
+JOIN wp207_posts wp ON wm.post_id = wp.ID 
+SET wm.meta_value='not available' 
+WHERE 
+    wm.meta_key='status' AND
+    wm.meta_value='available' AND
+    DATEDIFF(NOW(), wp.post_date) > 182";
+
+
+$final = $wpdb->query( $wpdb->prepare( 
+	"
+	UPDATE $wpdb->posts 
+	SET post_parent = %d
+	WHERE ID = %d
+		AND post_status = %s
+	",
+        7, 15, 'static'
+) );
+
+$post_id = $wpdb->get_results("SELECT post_id FROM $wpdb->postmeta WHERE (meta_key = 'mfn-post-link1' AND meta_value = '". $from ."')");
